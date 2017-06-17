6c31531beaf0c8fe81b1a7531de14944f9a57b74
Grantkempton13 <grantkempton13@yahoo.com>
2017-06-16 20:36:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("whatever").innerHTML = "Whatever We Want"
+
+document.getElementById("nickname").innerHTML = "GKemp"
+
+document.getElementById("favorite").innerHTML = "Coding"
+
+document.getElementById("hometown").innerHTML = "Des Moines, IA"
+
+document.body.style.background = "red"
+
+document.getElementById("whatever").style.backgroundColor = "blue";
+document.getElementById("whatever").style.color = "white";
+//document.getElementById("whatever").style.
+
+//var el = document.createElement('div');
+//el. innerHTML = '<p>Hello World</p>'
+
+//document.body.appendChild(el);
+
+
+//var el2 = document.querySelector('div');
+//var newEl = document.createElement('p');
